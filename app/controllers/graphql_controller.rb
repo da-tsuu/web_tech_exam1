82f7ef1a7a4e5864a7eec03d9a89a97bbade2ec5
takeru tsuda <da_tsuudayo@yahoo.co.jp>
2019-12-18 04:14:46 +0000

    pencil:

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -11,8 +11,7 @@ class GraphqlController < ApplicationController
         result = NginxAppSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
         render json: result
       end
-      private
-      # Handle form data, JSON body, or a blank value
+      
       def ensure_hash(ambiguous_param)
         case ambiguous_param
         when String
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :blogs
-  get 'graphiql', to: 'graphql#execute'
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
   
 end
